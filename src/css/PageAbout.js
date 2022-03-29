ff79ed288d52f83f93db742a63836ec39999d83b
Vincenzofdg <vfdgiacomo@gmail.com>
2022-03-29 11:41:27 -0300

    About First Container

--- src/css/PageAbout.js
+++ src/css/PageAbout.js
@@ -1,30 +1,47 @@
 import styled from "styled-components";
 
-const css = styled.main`
-  height: 93vh;
+const css = styled.main` 
+  height: 90%;
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
 
-  .upper-container {
-    height: 25vh;
-    font-size: 2rem;
+  .up-container {
+    height: 35%;
     display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
+    flex-direction: row;
+    justify-content: center;
 
-    h2 {
-      font-weight: bolder;
-      font-size: 3rem;
-    }
+    .image { padding: 2%; }
+    
+    .text {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
 
-    a {
-      font-weight: bold;
-      text-decoration: none;
+      h2 {
+        height: 20%;
+        font-weight: bolder;
+        //font-size: 3rem;
+      }
+
+      p {
+        height: 35%;
+        line-height: 1.7;
+        //font-size: 2.2rem;
+
+        a {
+          color: violet;
+          font-weight: bold;
+          text-decoration: none;
+        }
+      }
     }
   }
 
-  .lower-container {
+  .down-container {
+    //background: green;
     width: 100%;
     display: flex;
     justify-content: space-evenly;
@@ -67,6 +84,76 @@ const css = styled.main`
           height: 10%;
         }
       }
+    }
+
+    
+  }
+  // Responsividade
+  @media (min-width: 2035px) {
+    .text {
+      h2 { font-size: 3rem; }
+      p { font-size: 2.2rem; }
+    }
+  }
+  @media (max-width: 2035px) {
+    .image {
+      img { width: 250px }
+    }
+    .text {
+      padding: 1%;
+      h2 { font-size: 2.6rem; }
+      p { font-size: 2.1rem; }
+    }
+  }
+  @media (max-width: 1952px) {
+    .text {
+      h2 { font-size: 2.4rem; }
+      p { font-size: 1.9rem; }
+    }
+  }
+  @media (max-width: 1792px) {
+    .image {
+      img { width: 230px }
+    }
+      h2 { font-size: 2.2rem; }
+      p { font-size: 1.8rem; }
+    }
+  }
+  @media (max-width: 1690px) {
+    .image {
+      img { width: 200px }
+    }
+    .text {
+      h2 { font-size: 2rem; }
+      p { font-size: 1.6rem; }
+    }
+  }
+  @media (max-width: 1498px) {
+    .image {
+      img { width: 180px }
+    }
+    .text {
+      h2 { font-size: 1.9rem; }
+      p { font-size: 1.5rem; }
+    }
+  }
+  @media (max-width: 1395px) {
+    .image { display: none; }
+    .text {
+      h2 { font-size: 2rem; }
+      p { font-size: 1.6rem; }
+    }
+  }
+  @media (max-width: 625px) {
+    .text {
+      h2 { font-size: 1.8rem; }
+      p { font-size: 1.4rem; }
+    }
+  }
+  @media (max-width: 545px) {
+    .text {
+      h2 { font-size: 1.6rem; }
+      p { font-size: 1.2rem; }
     }
   }
 `;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react'
+
+import Footer from '../components/Footer';
 import MyContext from '../context/MyContext';
 import Header from '../components/Header';
-
 import PageAbout from '../css/PageAbout'
 import profile from '../img/profile.jpeg'
 
@@ -12,18 +13,24 @@ function About() {
     <>
       <Header />
         { language && (
+          <>
           <PageAbout>
-            <div className="upper-container">
-              <h2>Hey! my name is Vincenzo, I am a 28 years old developer from Brazil.</h2>
-              <p>
-                For the past year I have developed a fascination for technology and programming languages, so I decided to start studing Information Systems at <a href="https://www.unisanta.br/" target="blank">University Santa Cecilia</a> in Santos/Brazil.
-              </p>
-              <p>
-                I dropped university in <time><b>2021</b></time> because I expected that would be more classes about programming skills. Two mounths after dropping unisersity I started studing Web Development at <a href="https://www.betrybe.com/" target="blank">Trybe</a>.
-              </p>
+            <div className="up-container">
+              <div className="image">
+                <img src={ process.env.PUBLIC_URL + `/images/Computer.png` } alt="Icon Computer" />
+              </div>
+              <div className="text">
+                <h2>Hey! my name is Vincenzo, I am a 28 years old developer from Brazil.</h2>
+                <p>For the past year I have developed a fascination for technology and programming languages, so I decided to start studing Information Systems 
+                  at <a href="https://www.unisanta.br/" target="blank">University Santa Cecilia</a> in Santos/Brazil.
+                  <br />
+                  I dropped university in <time><b>2021</b></time> because I expected that would be more classes about programming skills. Two mounths after dropping 
+                  university I started studing Web Development at <a href="https://www.betrybe.com/" target="blank">Trybe</a>.</p>
+              </div>
             </div>
-            <div className="lower-container">
-                <img src={ profile } alt="Profile" />
+            
+            <div className="down-container">
+                <img src={ process.env.PUBLIC_URL + `/images/Skills.png` } alt="Profile" />
               <div className="skill-container">
                 <ul>
                   <p><strong>Soft Skills</strong></p>
@@ -45,19 +52,26 @@ function About() {
               </div>
             </div>
           </PageAbout>
+           <Footer />
+           </>
         )}
         { !language && (
+          <>
           <PageAbout>
-            <div className="upper-container">
+            <div className="up-container">
+              <div className="image">
+                <img src={ process.env.PUBLIC_URL + `/images/Computer.png` } alt="Icon Computer" />
+              </div>
+              <div className="text">
               <h2>Hey! meu nome é Vincenzo, Eu sou um desenvolvedor Brasileiro de 26 anos.</h2>
-              <p>
-              Nos ultimos anos eu criei um paixão e fascínio por tecnologia e programação, então eu decidi começar a estudar Sistema de Informação na <a href="https://www.unisanta.br/" target="blank">Universidade Santa Cecilia</a> em Santos/Brasil.
-              </p>
-              <p>
-                Eu acabei saindo da faculdade em <time><b>2021</b></time> pois eu entrei tendo a expectativa que haveria mais aulas voltadas à programação. Após dois meses de ter saido da Universidade eu começei a estudar Desenvolvimento Web na <a href="https://www.betrybe.com/" target="blank">Trybe</a>.
-              </p>
+                <p>Nos ultimos anos eu criei um paixão e fascínio por tecnologia e programação, então eu decidi começar a estudar Sistema de Informação 
+                  na <a href="https://www.unisanta.br/" target="blank">Universidade Santa Cecilia</a> em Santos/Brasil.
+                  <br />
+                  Eu acabei saindo da faculdade em <time><b>2021</b></time> pois eu entrei tendo a expectativa que haveria mais aulas voltadas à programação. Após dois 
+                  meses de ter saido da Universidade eu começei a <br />estudar Desenvolvimento Web na <a href="https://www.betrybe.com/" target="blank">Trybe</a>.</p>
+              </div>
             </div>
-            <div className="lower-container">
+            <div className="down-container">
                 <img src={ profile } alt="Profile" />
               <div className="skill-container">
                 <ul>
@@ -80,6 +94,8 @@ function About() {
               </div>
             </div>
           </PageAbout>
+          <Footer />
+          </>
         )}
     </>
   )
